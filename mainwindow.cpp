2d898848158a041ab1d7fa1d0e1b8e1b2660e1e0
lord-of-nothing <77851847+lord-of-nothing@users.noreply.github.com>
2024-10-01 02:56:32 +0000

    add first task

---
+++ mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->copyTextButton, &QPushButton::released, this, &MainWindow::moveText);
+    ui->stickyButton->setCheckable(true);
+    connect(ui->stickyButton, &QPushButton::released, this, [this](){qWarning() << "lower button pushed";});
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::moveText() {
+    qWarning() << "upper button clicked";
+    QString data = ui->textInput->toPlainText();
+    qWarning() << "text: " << data;
+    ui->textOutput->setPlainText(data);
+}
